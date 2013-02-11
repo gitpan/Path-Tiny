dcafe767ef647d789004a618a259353244e3b8a1
David Golden <dagolden@cpan.org>
2013-02-11 18:22:33 +0000

    Import of DAGOLDEN/Path-Tiny-0.006 from CPAN.
    gitpan-cpan-distribution: Path-Tiny
    gitpan-cpan-version:      0.006
    gitpan-cpan-path:         DAGOLDEN/Path-Tiny-0.006.tar.gz
    gitpan-cpan-author:       DAGOLDEN
    gitpan-cpan-maturity:     released

--- lib/Path/Tiny.pm
+++ lib/Path/Tiny.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Path::Tiny;
 # ABSTRACT: File path utility
-our $VERSION = '0.005'; # VERSION
+our $VERSION = '0.006'; # VERSION
 
 # Dependencies
 use autodie 2.00;
@@ -57,7 +57,7 @@ sub path {
 }
 
 
-sub new { path( $_[1] ) }
+sub new { shift; path( @_ ) }
 
 
 sub rootdir { path( File::Spec->rootdir ) }
@@ -419,7 +419,7 @@ Path::Tiny - File path utility
 
 =head1 VERSION
 
-version 0.005
+version 0.006
 
 =head1 SYNOPSIS
 
